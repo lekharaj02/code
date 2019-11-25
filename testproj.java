c11a3ae79284790a4c6ab78b0466f6bcffad3d2d
lekharaj02 <56316241+lekharaj02@users.noreply.github.com>
2019-11-25 16:04:44 +0530

    Update testproj.java

--- testproj.java
+++ testproj.java
@@ -2,7 +2,7 @@ public class testproj {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("testprogram for build2");
+		System.out.println("testprogram for build3");
 	}
 
 }
